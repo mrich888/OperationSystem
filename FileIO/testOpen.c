d9e2660883cce9416b09c3e251de5ec58d259eef
mrich888 <850338256@qq.com>
2024-01-04 16:21:33 +0800

    [修改] 验证文件描述符占用的是空闲的最小的一个

--- FileIO/testOpen.c
+++ FileIO/testOpen.c
@@ -9,12 +9,40 @@
 int main()
 {
     /* 打开文件 */
-    int fd = open("./hello.txt", O_RDWR | O_CREAT, 0644); // O_RDWR | O_CREA可选和必选
-    if (fd == -1)
+    int fd1 = open("./hello.txt", O_RDWR | O_CREAT, 0644); // O_RDWR | O_CREA可选和必选
+    if (fd1 == -1)
     {
         perror("opnen error");
     }
-    /* 要及时关闭文件 */
-    close(fd);
+    printf("fd1 = %d\n", fd1);
+    int fd2 = open("./hello.txt", O_RDWR | O_CREAT, 0644); // O_RDWR | O_CREA可选和必选
+    if (fd2 == -1)
+    {
+        perror("opnen error");
+    }
+    printf("fd2 = %d\n", fd2);
+    int fd3 = open("./hello.txt", O_RDWR | O_CREAT, 0644); // O_RDWR | O_CREA可选和必选
+    if (fd3 == -1)
+    {
+        perror("opnen error");
+    }
+    printf("fd3 = %d\n", fd3);
+    int fd4 = open("./hello.txt", O_RDWR | O_CREAT, 0644); // O_RDWR | O_CREA可选和必选
+    if (fd4 == -1)
+    {
+        perror("opnen error");
+    }
+    printf("fd4 = %d\n", fd4);
+  
+    /* 要及时关闭文件fd1 */
+    close(fd1);
+
+    int fd5 = open("./hello.txt", O_RDWR | O_CREAT, 0644); // O_RDWR | O_CREA可选和必选
+    if (fd5 == -1)
+    {
+        perror("opnen error");
+    }
+    printf("fd5 = %d\n", fd5);
+
     return 0;
 }
